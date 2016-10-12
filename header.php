a75d5bdf00e70840d183b824ff29fb2d143a9adc
marlos carmo <marlos@gmail.com>
2016-10-11 21:16:56 -0300

    page speed

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="Expires" content="30" />
     <title>marloscarmo.com - development, project management & audience</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/lib.css">
