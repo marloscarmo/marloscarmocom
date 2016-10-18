f833b6a0d935579cf8071c1582bb047b6838b74c
marlos carmo <marlos@gmail.com>
2016-10-18 21:32:27 -0200

    fix css

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/lib.css">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
   </head>
-  <body>
+  <body <?php body_class(); ?>>
 
     <header class="header-orange">
       <div class="brand">
